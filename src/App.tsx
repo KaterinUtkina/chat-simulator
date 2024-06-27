26fe3bccc2d073f889c6b389e208b3839a43ce84
KaterinUtkina <kate1205.96@mail.ru>
2024-06-27 08:52:48 +0300

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 function App() {
     return (
-        <main></main>
+        <main>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </main>
     )
 }
 
